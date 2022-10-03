5785021dd94c8590392d5f4362d9cc249442198d
Henry Smith <work.henrysmith@gmail.com>
2022-10-03 20:10:14 +0700

    create the CartCounting feature, using useState

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import CartCounting from './Hook-State';
 
 function App() {
   return (
     <div className="App">
       <h1>Learning React Hooks!</h1>
+      <CartCounting />
     </div>
   );
 }
---
+++ src/Hook-State.js
@@ -0,0 +1,28 @@
+import {useState} from 'react';
+import './App.css';
+
+const CartCounting = () => {
+    const [counter, setCounter] = useState(0);
+
+    const AddHandle = () => {
+        setCounter(counter + 1);
+    };
+
+    const RemoveHandle = () => {
+        if (counter === 0) {
+            setCounter(0);
+        } else {
+            setCounter(counter - 1);
+        }
+    };
+
+    return(
+        <div>
+            <h2>{counter}</h2>
+            <button className='button' onClick={RemoveHandle}>Remove Item</button>
+            <button className='button' onClick={AddHandle}>Add Item</button>
+        </div>
+    )
+};
+
+export default CartCounting;
